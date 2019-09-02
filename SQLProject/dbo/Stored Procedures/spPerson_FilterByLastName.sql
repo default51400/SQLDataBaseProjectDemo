d6e42ee5b20cd7da06052f3b20efec5291f81c21
Oleksii Petrov <opetrov2@softserveinc.com>
2019-09-02 13:22:10 +0300

    Add project files.

---
+++ SQLProject/dbo/Stored Procedures/spPerson_FilterByLastName.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spPerson_FilterByLastName]
+	@LastName nvarchar(50)
+AS
+begin
+	SELECT [PersonId], [FirstName], [LastName]
+	from dbo.Person
+	where LastName = @LastName;
+end
